f11871b7ef862adb6da18acd2d10aa39e90d8050
kealuya <kealuya@126.com>
2018-06-21 08:42:43 +0800

    1

--- src/main/java/cn/com/szht/config/exception/SzhtExceptionHandler.java
+++ src/main/java/cn/com/szht/config/exception/SzhtExceptionHandler.java
@@ -5,9 +5,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
